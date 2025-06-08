7f7352d2020f8233a186accf23ebc76c2733dd35
LucThomass <ethio.ethic@orange.fr>
2025-06-08 21:10:02 +0200

    Update signup.js failed

--- RestoLeQuaiAntique/js/auth/signup.js
+++ RestoLeQuaiAntique/js/auth/signup.js
@@ -5,26 +5,54 @@ const inputPrenom = document.getElementById("PrenomInput");
 const inputMail = document.getElementById("EmailInput");
 const inputPassword = document.getElementById("PasswordInput");
 const inputValidationPassword = document.getElementById("ValidatePasswordInput");
+const btnvalidation = document.getElementById("btn-validation-inscription");
 
-inputNom.addEventListener("keyup", validateForm); 
+inputNom.addEventListener("keyup", validateForm);
 inputPrenom.addEventListener("keyup", validateForm);
 inputMail.addEventListener("keyup", validateForm);
 inputPassword.addEventListener("keyup", validateForm);
 inputValidationPassword.addEventListener("keyup", validateForm);
 
 //Function permettant de valider tout le formulaire
-function validateForm(){
-    validateRequired(inputNom);
-    validateRequired(inputPrenom);
+function validateForm() {
+    const nomOk = validateRequired(inputNom);
+    const prenomOk = validateRequired(inputPrenom);
+    const mailOk = validateMail(inputMail);
+    
+    if (nomOk && prenomOk && mailOk) {
+        btnvalidation.disabled = false;
+    }
+    else {
+        btnvalidation.disabled = true;
+    }
+
 }
 
-function validateRequired(input){
-    if(input.value != ''){
+function validateMail(input) {
+    //Définir mon regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const mailUser = input.value;
+    if (mailUser.match(emailRegex)) {
         input.classList.add("is-valid");
-        input.classList.remove("is-invalid"); 
+        input.classList.remove("is-invalid");
+        return true;
     }
-    else{
+    else {
         input.classList.remove("is-valid");
         input.classList.add("is-invalid");
+        return false;
+    }
+}
+
+function validateRequired(input) {
+    if (input.value != '') {
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+        return true;
+    }
+    else {
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        return false;
     }
 }
