121173a7059c32745f5fd03e9eac08907820f9fd
LucThomass <ethio.ethic@orange.fr>
2025-06-11 19:26:47 +0200

    Valid MdP en cours

--- RestoLeQuaiAntique/js/auth/signup.js
+++ RestoLeQuaiAntique/js/auth/signup.js
@@ -6,7 +6,7 @@ const inputMail = document.getElementById("EmailInput");
 const inputPassword = document.getElementById("PasswordInput");
 const inputValidationPassword = document.getElementById("ValidatePasswordInput");
 const btnvalidation = document.getElementById("btn-validation-inscription");
-
+const passwordConfirmOk = validateConfirmationPassword(inputPassword, inputValidationPassword);
 
 inputNom.addEventListener("keyup", validateForm);
 inputPrenom.addEventListener("keyup", validateForm);
@@ -21,7 +21,7 @@ function validateForm() {
     const mailOk = validateMail(inputMail);
     const PasswordOK = validatePassword(inputPassword);
 
-    if (nomOk && prenomOk && mailOk && PasswordOK) {
+    if (nomOk && prenomOk && mailOk && PasswordOK && passwordConfirmOk) {
         btnvalidation.disabled = false;
     }
     else {
@@ -46,21 +46,34 @@ function validateMail(input) {
     }
 }
 
-function validatePassword(input){
+function validateConfirmationPassword(inputPwd, inputConfirmPwd) {
+    if (inputPwd.value == inputConfirmPwd.value) {
+        inputConfirmPwd.classList.add("is-valid");
+        inputConfirmPwd.classList.remove("is-invalid");
+        return true;
+    } else {
+        inputConfirmPwd.classList.add("is-invalid");
+        inputConfirmPwd.classList.remove("is-valid");
+        return false;
+    }
+}
+
+function validatePassword(input) {
     //Définir mon regex
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
     const passwordUser = input.value;
-    if(passwordUser.match(passwordRegex)){
+    if (passwordUser.match(passwordRegex)) {
         input.classList.add("is-valid");
-        input.classList.remove("is-invalid"); 
+        input.classList.remove("is-invalid");
         return true;
     }
-    else{
+    else {
         input.classList.remove("is-valid");
         input.classList.add("is-invalid");
         return false;
     }
 }
+
 
 function validateRequired(input) {
     if (input.value != '') {
