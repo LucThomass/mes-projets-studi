4176eef8eb33fed98e4355597297d6ed17cdebc6
LucThomass <ethio.ethic@orange.fr>
2025-06-12 13:06:46 +0200

    cookie en cours

--- RestoLeQuaiAntique/js/auth/signin.js
+++ RestoLeQuaiAntique/js/auth/signin.js
@@ -6,11 +6,20 @@ btnSignin.addEventListener('click', checkCredentials);
 
 function checkCredentials() {
     // Récupération des valeurs des champs de saisie factices pour l'instant "credentials en BDD"//
+
     if (mailInput.value == "test@mail.com" && passwordInput.value == "123") {
-        alert("Connexion réussie !");
         // Redirection vers la page d'accueil après une connexion réussie
         const token = "fakenvkld!fnùdwnbkl!ndfblkndkwlù"
-        
+        setToken(token);
+
+//Il faudra récupérer le vrai token
+        //const token = "lkjsdngfljsqdnglkjsdbglkjqskjgkfjgbqslkfdgbskldfgdfgsdgf";
+        //setToken(token);
+        //placer ce token en cookie
+
+        //setCookie(RoleCookieName, "admin", 7);//
+
+
         window.location.replace('/');
     }
     else {
