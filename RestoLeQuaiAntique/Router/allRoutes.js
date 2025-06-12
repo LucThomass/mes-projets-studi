4b0ddaf937e810c158049d41d9415e4bb0e624fe
LucThomass <ethio.ethic@orange.fr>
2025-06-12 21:17:15 +0200

    Dyn Front terminé

--- RestoLeQuaiAntique/Router/allRoutes.js
+++ RestoLeQuaiAntique/Router/allRoutes.js
@@ -9,7 +9,7 @@ export const allRoutes = [
     new Route("/account", "Mon compte",  "/pages/auth/account.html", ["client", "admin"],),
     new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", ["client", "admin"],),
     new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html", ["client"]),
-    new Route("/reserver", "Réserver", "/pages/reservations/reserver.html", ["client"]),
+    new Route("/reserver", "Réserver", "/pages/reservations/reserver.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
