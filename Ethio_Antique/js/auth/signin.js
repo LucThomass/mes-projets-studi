3eca562ab6deab88eda0ea4683fbe74bc20bc479
LucThomass <ethio.ethic@orange.fr>
2025-07-30 20:04:55 +0200

    Ethio Antique

---
+++ Ethio_Antique/js/auth/signin.js
@@ -0,0 +1,29 @@
+const mailInput = document.getElementById('EmailInput');
+const passwordInput = document.getElementById('PasswordInput');
+const btnSignin = document.getElementById('btnSignin');
+
+btnSignin.addEventListener('click', checkCredentials);
+
+function checkCredentials() {
+    // Récupération des valeurs des champs de saisie factices pour l'instant "credentials en BDD"//
+
+    if (mailInput.value == "test@mail.com" && passwordInput.value == "123") {
+        // Redirection vers la page d'accueil après une connexion réussie
+        const token = "fakenvkld!fnùdwnbkl!ndfblkndkwlù"
+        setToken(token);
+
+//Il faudra récupérer le vrai token
+        //const token = "lkjsdngfljsqdnglkjsdbglkjqskjgkfjgbqslkfdgbskldfgdfgsdgf";
+        //setToken(token);
+        //placer ce token en cookie
+
+        setCookie(RoleCookieName, "client", 7);
+
+
+        window.location.replace('/');
+    }
+    else {
+        mailInput.classList.add("is-invalid");
+        passwordInput.classList.add("is-invalid");
+    }
+}
